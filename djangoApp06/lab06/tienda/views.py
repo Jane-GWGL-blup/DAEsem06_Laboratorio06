3070c63ab6e2c0e79ff88357b9dd11b125b97a12
Nicole Caceres <janenieggcs@gmail.com>
2022-04-19 18:53:39 -0500

    Primer commit

---
+++ djangoApp06/lab06/tienda/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Producto, Categoria
+# Create your views here.
+def index(request):
+    product_list = Producto.objects.order_by('nombre')[:6]
+    category_list = Categoria.objects.order_by('nombre')
+    context = {'product_list':product_list,'category_list':category_list}
+    return render(request, 'index.html', context)
+
+def producto(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+    category_list = Categoria.objects.order_by('nombre')
+    return render(request, 'producto.html', {'producto': producto, 'category_list':category_list})
+
+def categoria(request, categoria_id):
+    products = Producto.objects.filter(categoria=categoria_id)
+    category_list = Categoria.objects.order_by('nombre')
+    return render(request, 'categoria.html', {'products': products, 'category_list':category_list})
